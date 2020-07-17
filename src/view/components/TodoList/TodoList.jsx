ab5a1dcafe02a8f4294153b6982bc2f205097bcc
VLAD SEGEDA <vladcomart@gmail.com>
2020-07-17 18:20:34 +0300

    wip feature/localStorage

--- src/view/components/TodoList/TodoList.jsx
+++ src/view/components/TodoList/TodoList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback} from "react";
 import List from "@material-ui/core/List";
 import {ListItem} from "@material-ui/core";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -6,26 +6,36 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from '@material-ui/icons/Delete';
+import {isEmpty} from "lodash";
 
-export const Todolist = ({todos, deleteTodo}) => {
+export const Todolist = ({localTodos, deleteTodo}) => {
+
+    const renderTodoItem = useCallback(() => {
+        if (isEmpty(localTodos)){
+            return null
+        }
+
+        return localTodos.map((todo, index) => (
+            <ListItem key={index.toString()} dense button>
+                <Checkbox tabIndex={-1} disableRipple/>
+                <ListItemText primary={todo}/>
+                <ListItemSecondaryAction>
+                    <IconButton
+                        aria-label="Delete"
+                        onClick={() => {
+                            deleteTodo(index);
+                        }}
+                    >
+                        <DeleteIcon/>
+                    </IconButton>
+                </ListItemSecondaryAction>
+            </ListItem>
+        ))
+    }, [deleteTodo, localTodos]);
+
     return (
         <List style={{width: '500px'}}>
-            {todos.map((todo, index) => (
-                <ListItem key={index.toString()} dense button>
-                    <Checkbox tabIndex={-1} disableRipple />
-                    <ListItemText primary={todo} />
-                    <ListItemSecondaryAction>
-                        <IconButton
-                            aria-label="Delete"
-                            onClick={() => {
-                                deleteTodo(index);
-                            }}
-                        >
-                            <DeleteIcon />
-                        </IconButton>
-                    </ListItemSecondaryAction>
-                </ListItem>
-            ))}
+            {renderTodoItem()}
         </List>
     )
 }
--- src/view/pages/Home/Home.jsx
+++ src/view/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {TodoForm} from "../../components/TodoForm/TodoForm";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
@@ -8,6 +8,13 @@ import Card from "@material-ui/core/Card";
 export const Home = () => {
 
     const [todos, setTodos] = useState([]);
+    const todosSerialized = JSON.stringify(todos);
+    const todosDeserialized = JSON.parse(localStorage.getItem('todos'));
+    useEffect(() => {
+
+    }, []);
+
+
 
     const saveTodoItem = useCallback((todoText) => {
         const trimmedText = todoText.trim();
@@ -15,23 +22,32 @@ export const Home = () => {
         if (trimmedText.length > 0) {
             setTodos([...todos, trimmedText]);
         }
-    }, [todos])
+
+        localStorage.setItem('todos', todosSerialized)
+    }, [todos, todosSerialized])
 
     const deleteTodoItem = useCallback((todoIndex) => {
         const newTodos = todos.filter((_, index) => index !== todoIndex);
 
         setTodos(newTodos);
+
     }, [todos])
 
     return (
         <Grid container item xs={12} sm={12} alignItems='center' alignContent='center' direction='column'
               justify='center'>
-            <Card variant='outlined' style={{display: 'flex', alignItems: 'center', alignContent: 'center',justifyContent: 'center', flexDirection: 'column' }}>
+            <Card variant='outlined' style={{
+                display: 'flex',
+                alignItems: 'center',
+                alignContent: 'center',
+                justifyContent: 'center',
+                flexDirection: 'column'
+            }}>
                 <Typography variant='h1' color='textPrimary'>Todos</Typography>
                 <TodoForm
                     saveTodo={saveTodoItem}/>
                 <Todolist
-                    todos={todos}
+                    localTodos={todosDeserialized}
                     deleteTodo={deleteTodoItem}/>
             </Card>
         </Grid>
