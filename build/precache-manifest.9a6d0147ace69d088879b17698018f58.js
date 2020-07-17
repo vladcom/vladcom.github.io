a5a373e22136b817294395770ec3c1f38194eec0
VLAD SEGEDA <vladcomart@gmail.com>
2020-07-17 18:15:34 +0300

    build

---
+++ build/precache-manifest.9a6d0147ace69d088879b17698018f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e06259f7b4c9a34d7706c3137be8fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fbd6ad733dab1b3ac12",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed8a9865b437693b39fd",
+    "url": "/static/js/2.797fbcd9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.797fbcd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fbd6ad733dab1b3ac12",
+    "url": "/static/js/main.ceea2def.chunk.js"
+  },
+  {
+    "revision": "45d62cc8786fead2ed2f",
+    "url": "/static/js/runtime-main.b3475604.js"
+  }
+]);
